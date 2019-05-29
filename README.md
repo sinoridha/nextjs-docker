656b80a49d7f5cf0bed4ebe65b61091ec23e6ef0
Ridha Ramadhansyah <ridha.ramadhansyah@sehatq.com>
2019-05-29 11:39:52 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,30 @@
+# nextjs-docker
+
+This is initial project next.js with docker.
+
+Specification :
+* start with docker image node:10 (node version v10.15.3)
+* react 16.8.4
+* next.js 8.0.3
+
+## Prerequisite
+
+Install docker on your local machine.
+
+## Run Local
+
+Run on port 3000 in your local machine.
+
+```
+$ ./build-local
+```
+
+
+## Run Build Production
+
+
+Run on port 3000 for build production
+
+```
+$ ./build-server
+```
---
+++ pages/index.js
@@ -0,0 +1,46 @@
+import Layout from '../components/MyLayout.js'
+import Link from 'next/link'
+import fetch from 'isomorphic-unfetch'
+import Head from 'next/head'
+
+function PostLink(props) {
+  return (
+    <li>
+      <Link as={`/p/${props.id}.html`} href={`/post?title=${props.title}`}>
+        <a>{props.title}</a>
+      </Link>
+    </li>
+  )
+}
+
+function Index(props) {
+  console.log('shows: ', props.shows);
+  return (
+    <div>
+    <Head>
+      <title>{props.shows[0].show.name}</title>
+    </Head>
+    <Layout>
+      <h1>My Blog</h1>
+      <ul>
+        <PostLink id="hello-nextjs" title="Hello Next.js" />
+        <PostLink id="learn-nextjs" title="Learn Next.js is awesome" />
+        <PostLink id="deploy-nextjs" title="Deploy apps with Zeit" />
+      </ul>
+    </Layout>
+    </div>
+  )
+}
+
+Index.getInitialProps = async function() {
+  const res = await fetch('http://api.tvmaze.com/search/shows?q=batman')
+  const data = await res.json()
+
+  console.log(`Show data fetched. Count: ${data.length}`)
+
+  return {
+    shows: data
+  }
+}
+
+export default Index

060193d4b5f217855f1f12155438e2946a36f054
Ridha Ramadhansyah <ridha.ramadhansyah@sehatq.com>
2019-05-31 08:40:14 +0700

    add eslint

--- components/Header.js
+++ components/Header.js
@@ -1,18 +1,18 @@
 import Link from 'next/link'
 
 const linkStyle = {
-  marginRight: 15
+    marginRight: 15
 }
 
 export default function Header() {
-  return (
-    <div>
-      <Link href="/">
-        <a style={linkStyle}>Home</a>
-      </Link>
-      <Link href="/about">
-        <a style={linkStyle}>About</a>
-      </Link>
-    </div>
-  )
+    return (
+        <div>
+            <Link href="/">
+                <a style={linkStyle}>Home</a>
+            </Link>
+            <Link href="/about">
+                <a style={linkStyle}>About</a>
+            </Link>
+        </div>
+    )
 }
